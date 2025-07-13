59d287ed95717c3a57bdd579ca9821481e663490
Petrica <petrica.avornicesei@student.tuiasi.ro>
2025-07-13 09:56:27 +0300

    (feat.) Simple player movement

--- AA2/src/AA2_Player.cpp
+++ AA2/src/AA2_Player.cpp
@@ -1,4 +1,5 @@
 #include "AA2_Player.h"
+#include <iostream>
 
 AA2_Player::AA2_Player(AA2_GraphicsContext *p_graphics_context, SDL_FRect *p_data) : AA2_Creature(p_graphics_context, p_data)
 {
@@ -17,7 +18,12 @@ void AA2_Player::Init(const char *texture_path)
 
 void AA2_Player::Update()
 {
-    data->x += 0.1;
+    const bool *keystates = SDL_GetKeyboardState(NULL);
+
+    if (keystates[SDL_SCANCODE_W]) data->y -= speed;
+    if (keystates[SDL_SCANCODE_S]) data->y += speed;
+    if (keystates[SDL_SCANCODE_A]) data->x -= speed;
+    if (keystates[SDL_SCANCODE_D]) data->x += speed;
 }
 
 void AA2_Player::Render()
